06f6a1462d1bd8fb56c56e9d14bb2ef1adca22dc
Dio-1rom11 <dasromit12@gmail.com>
2024-12-24 20:21:41 -0500

    Added a Tooltip for the brush

--- src/main/java/net/romit/simpleedits/item/custom/BrushItem.java
+++ src/main/java/net/romit/simpleedits/item/custom/BrushItem.java
@@ -5,6 +5,8 @@ import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -13,6 +15,8 @@ import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockPos.Mutable;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 
 public class BrushItem extends Item {
@@ -80,5 +84,12 @@ public class BrushItem extends Item {
 
     public static void setBrushShape(String shape) {
         brushShape = shape;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("tooltip.simpleedits.brush.tooltip"));
+
+        super.appendTooltip(stack, context, tooltip, type);
     }
 }
